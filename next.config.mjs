c261e9cacc88533da13e5694ebfc626b5345fc74
akihiro <liid05754@gmail.com>
2024-11-06 22:21:28 +0300

    feat: some feats in next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ["tonkeeper.com", "static.mytonwallet.io"],
   },
+  eslint: {
+    dirs: [],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
